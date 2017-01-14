9558a8256322a64e6c364405bdbe7b4556ad79e7
Martijn van Dorp <martijnvandorp@casema.nl>
2017-01-14 20:46:51 +0100

    Remove -ip parameter when starting srcds, set ip in server_private.cfg instead

--- start-servers.sh
+++ start-servers.sh
@@ -10,11 +10,10 @@ PARISH=c5m1_waterfront
 NO_MERCY=c8m1_apartment
 
 DEFAULT_MAP=$PARISH
-SERVER_IP=""
 
-screen -mdS server01 taskset -c 0 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27015 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_01
-screen -mdS server02 taskset -c 1 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27016 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_02
-screen -mdS server03 taskset -c 2 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27017 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_03
-screen -mdS server04 taskset -c 3 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27018 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_04
-screen -mdS server05 taskset -c 4 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27019 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_05
-screen -mdS server06 taskset -c 5 "$SERVER_PATH" -game left4dead2 -tickrate 100 -ip $SERVER_IP -port 27020 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_06
+screen -mdS server01 taskset -c 0 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27015 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_01
+screen -mdS server02 taskset -c 1 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27016 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_02
+screen -mdS server03 taskset -c 2 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27017 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_03
+screen -mdS server04 taskset -c 3 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27018 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_04
+screen -mdS server05 taskset -c 4 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27019 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_05
+screen -mdS server06 taskset -c 5 "$SERVER_PATH" -game left4dead2 -tickrate 100 -port 27020 +map $DEFAULT_MAP +mp_gamemode "versus" +exec server_id_06
